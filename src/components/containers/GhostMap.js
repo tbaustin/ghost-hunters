dc75f9944e06aaaf6c259a05c8bf55f6bbbf95c7
Taylor <tbaustin1992@gmail.com>
2017-11-26 13:19:54 -0600

    removed google api key

--- src/components/containers/GhostMap.js
+++ src/components/containers/GhostMap.js
@@ -14,7 +14,7 @@ import { MarkerInfo } from '../view';
 
 const Map = compose(
   withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=AIzaSyD74U-V5x5WvAgtkfWBxd7WERdVZFRI_dk&v=3.exp&libraries=geometry,drawing,places`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `500px` }} />,
     mapElement: <div style={{ height: `100%` }} />
